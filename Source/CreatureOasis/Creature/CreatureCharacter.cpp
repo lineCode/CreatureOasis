c6eb388f396df994f38318dbf261c577ef22eb0d
Dion van der Wateren <39389180+Nidolgh@users.noreply.github.com>
2022-01-03 23:13:21 +0100

    Changed values regarding smoothly rotating the creature

--- Source/CreatureOasis/Creature/CreatureCharacter.cpp
+++ Source/CreatureOasis/Creature/CreatureCharacter.cpp
@@ -3,12 +3,17 @@
 
 #include "CreatureCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
 // Sets default values
 ACreatureCharacter::ACreatureCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-    
+
+	bUseControllerRotationYaw = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 // Called when the game starts or when spawned
