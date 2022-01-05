07c24ffbd0fa67d0dff5211e1b6f12df715c2370
Dion van der Wateren <39389180+Nidolgh@users.noreply.github.com>
2022-01-05 22:40:22 +0100

    Added a Fruit that generates itself from a data table
    Added
     - FruitTableRow.h
     - BP_Fruit_Base that changes its behavior in a data driven way using data tables and the construction script
     - Placeholder Gameplay effect for 'bites'
    
    Updated
     - Neutral Nut now has correct scale and textures applied

---
+++ Source/CreatureOasis/Chao/Structs/FruitTableRow.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GameplayTagContainer.h"
+#include "FruitTableRow.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct CREATUREOASIS_API FFruitTableRow : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="FruitRowBase")
+	FGameplayTag GameplayTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="FruitRowBase")
+	UStaticMesh* StaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="FruitRowBase")
+	FVector MeshScaleOverride = FVector(1.f);
+	
+	// The amount of "bites" a creature can take from this fruit
+	UPROPERTY(EditAnywhere, Category="FruitStats")
+	int32 MaxPortions = 4;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="FruitGameplayEffects")
+	TSubclassOf<UGameplayEffect> PerBiteGameplayEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="FruitGameplayEffects")
+	TSubclassOf<UGameplayEffect> OnDepletedGameplayEffect;
+};
