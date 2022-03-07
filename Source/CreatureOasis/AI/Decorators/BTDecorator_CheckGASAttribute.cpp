4adfc0e5f35d329d3f112bff006355bd2fcabb45
Dion van der Wateren <39389180+Nidolgh@users.noreply.github.com>
2022-03-07 01:54:40 +0100

    Replaced the BP CheckGASProperty Decorator with one implemented in CPP, added additional functionality
    [Added]
     - BTDecorator_CheckGASAttribute cpp class
    
    [Updated]
     - BT_ChaoBehavior to now utilize the new CheckGASAttribute decorator class
    
    [Removed]
     - BTD_CheckGASProperty uasset in favor for a cpp equivalent

---
+++ Source/CreatureOasis/AI/Decorators/BTDecorator_CheckGASAttribute.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BTDecorator_CheckGASAttribute.h"
+
+#include "AbilitySystemComponent.h"
+#include "AIController.h"
+#include "CreatureOasis/GameplayAbilitySystem/GASCharacter.h"
+
+UBTDecorator_CheckGASAttribute::UBTDecorator_CheckGASAttribute()
+{
+	NodeName = TEXT("CheckGASAttribute");
+
+	bNotifyActivation	= false;
+	bNotifyDeactivation = false;
+	bNotifyProcessed	= false;
+}
+
+bool UBTDecorator_CheckGASAttribute::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	if (TargetAttribute.IsValid() && IsValid(OwnerComp.GetAIOwner()))
+	{
+		const AGASCharacter* GASCharacter = Cast<AGASCharacter>(OwnerComp.GetAIOwner()->GetPawn());
+		if(IsValid(GASCharacter))
+		{
+			const UAbilitySystemComponent* AbilitySystemComponent = GASCharacter->GetAbilitySystemComponent();
+			return CommenceComparativeCheck(AbilitySystemComponent->GetNumericAttributeBase(TargetAttribute));
+		}
+	}
+
+	return false;
+}
+
+bool UBTDecorator_CheckGASAttribute::CommenceComparativeCheck(const float ValToCheck) const
+{
+	bool CompareCheck = false;
+	
+	switch (CheckQuery)
+	{
+	case ECheckCompareQuery::Less:
+		CompareCheck = ComparativeVal < ValToCheck;
+		break;
+
+	case ECheckCompareQuery::LessOrEqual:
+		CompareCheck = ComparativeVal <= ValToCheck;
+		break;
+
+	case ECheckCompareQuery::Equal:
+		CompareCheck = ComparativeVal == ValToCheck;
+		break;
+
+	case ECheckCompareQuery::Greater:
+		CompareCheck = ComparativeVal > ValToCheck ;
+		break;
+
+	case ECheckCompareQuery::GreaterOrEqual:
+		CompareCheck = ComparativeVal >= ValToCheck;
+		break;
+		
+	default: ;
+	}
+	
+	return CompareCheck;
+}
---
+++ Source/CreatureOasis/AI/Decorators/BTDecorator_CheckGASAttribute.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_CheckGASAttribute.generated.h"
+
+/**
+ * Used for determining what kind of Comparative check we should use
+ */
+UENUM(BlueprintType)
+enum class ECheckCompareQuery : uint8
+{
+	Less				UMETA(DisplayName = "Less"),
+	LessOrEqual			UMETA(DisplayName = "LessOrEqual"),
+	Equal				UMETA(DisplayName = "Equal"),
+	Greater				UMETA(DisplayName = "Greater"),
+	GreaterOrEqual		UMETA(DisplayName = "GreaterOrEqual")
+};
+
+/**
+ * Use this Decorator node to check a GAS Attribute
+ */
+UCLASS()
+class CREATUREOASIS_API UBTDecorator_CheckGASAttribute : public UBTDecorator
+{
+	GENERATED_BODY()
+
+	UBTDecorator_CheckGASAttribute();
+	
+protected:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+	
+	bool CommenceComparativeCheck(const float ValToCheck) const;
+
+	UPROPERTY(EditAnywhere)
+	FGameplayAttribute TargetAttribute;
+
+	UPROPERTY(EditAnywhere)
+	float ComparativeVal;
+	
+	UPROPERTY(EditAnywhere)
+	ECheckCompareQuery CheckQuery;
+};
