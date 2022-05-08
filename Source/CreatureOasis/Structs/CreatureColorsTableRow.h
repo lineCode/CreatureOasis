7213f607cdb46445815e9c401c61f588e4009749
Dion van der Wateren <39389180+Nidolgh@users.noreply.github.com>
2022-05-08 17:29:14 +0200

    Moved and renamed a few structs

---
+++ Source/CreatureOasis/Structs/CreatureColorsTableRow.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "GameplayTagContainer.h"
+
+#include "CreatureColorsTableRow.generated.h"
+
+USTRUCT(BlueprintType)
+struct FCreatureColorsTableRow
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="CreatureColors")
+	FGameplayTag GameplayTagColor;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="CreatureColors")
+	FLinearColor PrimaryColor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="CreatureColors")
+	FLinearColor SecondaryColor;
+};
