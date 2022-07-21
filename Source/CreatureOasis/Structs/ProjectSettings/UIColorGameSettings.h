cdcc1c9d6880d78bb14458b76ed7f0dd92b70a95
Dion van der Wateren <39389180+Nidolgh@users.noreply.github.com>
2022-07-21 15:14:46 +0200

    Custom ProjectSettings test update

---
+++ Source/CreatureOasis/Structs/ProjectSettings/UIColorGameSettings.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UIColorGameSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config=Game, defaultconfig, meta = (DisplayName="Color Game Settings"))
+class CREATUREOASIS_API UUIColorGameSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category = "General")
+	int32 TestInt;
+
+	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category = "General")
+	TMap<FString, FColor> DummyColorAccents;
+};
