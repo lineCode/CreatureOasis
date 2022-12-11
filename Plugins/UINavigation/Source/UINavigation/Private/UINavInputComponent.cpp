12a61dfc0c821b9ffc18e054df13d0ffbf977dde
Nidolgh <nidolgh@gmail.com>
2022-12-11 22:15:47 +0300

    Removed UINavigation plugin directory and content

--- Plugins/UINavigation/Source/UINavigation/Private/UINavInputComponent.cpp
+++
@@ -1,4 +0,0 @@
-﻿// Copyright (C) 2019 Gonçalo Marques - All Rights Reserved
-
-#include "UINavInputComponent.h"
-
--- Plugins/UINavigation/Source/UINavigation/Private/UINavSettings.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (C) 2019 Gonçalo Marques - All Rights Reserved
-
-#include "UINavSettings.h"
-
-
-
-
